9431a71c7b2a81ad8fd6dcff8fb217f98e5439ed
Lê Song Vĩ <rappervn88@gmail.com>
2017-02-02 06:41:35 -0800

    Create getlink-pro2.js

---
+++ getlink-pro2.js
@@ -0,0 +1,80 @@
+function login()
+{
+user=document.dulieu.ten.value;
+if(user=="http://www.taive.ga/h6-teamviewer-12")
+{
+  window.location="http://download.teamviewer.com/download/TeamViewer_Setup_vi-ahxg.exe";
+}
+else
+user=document.dulieu.ten.value;
+if(user=="http://www.taive.ga/h7-upbeat-ukulele-background-music-that-positive-feeling-by-alumo")
+{
+  window.location="https://www.youtube.com/watch?v=m5F0Lq03spM";
+}
+else
+user=document.dulieu.ten.value;
+if(user=="http://www.taive.ga/h8-dejans-missing-you")
+{
+  window.location="https://www.youtube.com/watch?v=CAlMSBpfo0E";
+}
+else
+user=document.dulieu.ten.value;
+if(user=="http://www.taive.ga/h9-gta-sa.rar")
+{
+  window.location="http://www.mediafire.com/file/zhh0acu4u56c992/GTA+SA.rar";
+}
+user=document.dulieu.ten.value;
+if(user=="http://www.taive.ga/h10-kim-so-huyn.rar")
+{
+  window.location="http://www.mediafire.com/file/yg7kemobuypgpb7/Kim+So+Huyn.rar";
+}
+else
+user=document.dulieu.ten.value;
+if(user=="http://www.taive.ga/h11-mtasa-153.exe")
+{
+  window.location="https://community.multitheftauto.com/mirror/mtasa/main/mtasa-1.5.3.exe";
+}
+user=document.dulieu.ten.value;
+if(user=="http://www.taive.ga/h12-penitentiary-roleplay-031by-marco.rar")
+{
+  window.location="http://www.mediafire.com/file/if7ejfdxyfqnpwy/Penitentiary+Roleplay+0.3.1+by+Marco.rar";
+}
+else
+user=document.dulieu.ten.value;
+if(user=="http://www.taive.ga/h15-fifaonline3-20161208.rar")
+{
+  window.location="http://cdn.vn.garenanow.com/gamevn/fifaonline3/FIFAONLINE3_20161208.rar";
+}
+else
+user=document.dulieu.ten.value;
+if(user=="http://www.taive.ga/h17-theme-blog")
+{
+  window.location="http://github.com/BlackrockDigital/startbootstrap-clean-blog/archive/gh-pages.zip";
+}
+else
+user=document.dulieu.ten.value;
+if(user=="http://www.taive.ga/h18-hello-neighbor-alpha-3")
+{
+  window.location="http://www.lesongvi.ga/h214-bao-tri/";
+}
+else
+user=document.dulieu.ten.value;
+if(user=="http://www.taive.ga/h19-steamsetup.exe")
+{
+  window.location="http://steamcdn-a.akamaihd.net/client/installer/SteamSetup.exe";
+}
+else
+user=document.dulieu.ten.value;
+if(user=="http://www.taive.ga/h20-rld-gtav.iso")
+{
+  window.location="http://4share.vn/f/37040e030f0f0607/rld-gtav.iso";
+}
+else
+user=document.dulieu.ten.value;
+if(user=="http://www.taive.ga/h21-idman627build2.exe")
+{
+  window.location="http://mirror2.internetdownloadmanager.com/idman627build2.exe";
+}
+
+return false;
+}
